2d47a472f333b3d44ed2c5e598daa608b141a2ec
HungKD 20231113 <hungkhong_me@yahoo.com.vn>
2023-11-13 01:38:59 -0500

    Finish Budget Allocation Application

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
+import ExpenseList from './components/ExpenseList';
 
 // Add code to import the other components here under
 
@@ -17,18 +21,36 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <div className='row '>
+                                <h3 className='mt-3'>Allocation</h3>
+                                <div className='col-sm'>
+                                    <ExpenseList />
+                                </div>
+                            </div>
                         }         
 
                         {
@@ -37,6 +59,12 @@ const App = () => {
 
                         {
                             /* Add AllocationForm component here under */
+                            <div className='row mt-3'>
+                                <h3 className='mt-3'>Change allocation</h3>
+                                <div className='col-sm'>
+                                    <AllocationForm/>
+                                </div>
+                            </div>
                         }        
 
                 </div>
